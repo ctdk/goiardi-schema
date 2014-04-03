8551504a31a643982430e6935538c8e7383d217e
Jeremy Bingham <jbingham@gmail.com>
2014-04-02 21:36:30 -0700

    completed flag for sandboxes

--- mysql/deploy/sandboxes.sql
+++ mysql/deploy/sandboxes.sql
@@ -7,6 +7,7 @@ CREATE TABLE sandboxes (
 	sbox_id varchar(32) not null,
 	creation_time datetime not null,
 	checksums blob,
+	completed tinyint(4) default 0,
 	primary key(id),
 	unique key(sbox_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
