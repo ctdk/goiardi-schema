e620b0e697051ad55b5eb4b72b83c57864722feb
ctdk <jbingham@gmail.com>
2014-07-26 13:45:52 -0700

    Oops, add that convienient is_down flag to the latest node status view

--- postgres/verify/node_latest_statuses.sql
+++ postgres/verify/node_latest_statuses.sql
@@ -2,6 +2,6 @@
 
 BEGIN;
 
-SELECT id, name, chef_environment, run_list, automatic_attr, normal_attr, default_attr, override_attr, status, updated_at FROM goiardi.node_latest_statuses WHERE false;
+SELECT id, name, chef_environment, run_list, automatic_attr, normal_attr, default_attr, override_attr, is_down, status, updated_at FROM goiardi.node_latest_statuses WHERE false;
 
 ROLLBACK;
