59dd2dfb650048011adc3f3867e33d8adc59984b
ctdk <jbingham@gmail.com>
2014-08-27 01:42:49 -0700

    And now they pass various verifications

--- postgres/deploy/shovey_insert_update.sql
+++ postgres/deploy/shovey_insert_update.sql
@@ -22,19 +22,19 @@ END;
 $$
 LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION goiardi.merge_shovey_runs(m_shovey_run_id uuid, m_node_name text, m_status text, m_ack_time timestamp with time zone, m_end_time timestamp with time zone, m_output text, m_stderr text, m_exit_status smallint) RETURNS VOID AS
+CREATE OR REPLACE FUNCTION goiardi.merge_shovey_runs(m_shovey_run_id uuid, m_node_name text, m_status text, m_ack_time timestamp with time zone, m_end_time timestamp with time zone, m_output text, m_error text, m_stderr text, m_exit_status integer) RETURNS VOID AS
 $$
 DECLARE
     m_shovey_id bigint;
 BEGIN
     LOOP
-	UPDATE goiardi.shovey_runs SET status = m_status, ack_time = m_ack_time, end_time = m_end_time, output = m_output, stderr = m_stderr, exit_status = m_exit_status WHERE shovey_run_id = m_shovey_run_id AND node_name = m_node_name;
+	UPDATE goiardi.shovey_runs SET status = m_status, ack_time = m_ack_time, end_time = m_end_time, output = m_output, stderr = m_stderr, exit_status = cast(m_exit_status as smallint) WHERE shovey_run_id = m_shovey_run_id AND node_name = m_node_name;
 	IF found THEN
 	    RETURN;
 	END IF;
 	BEGIN
-	    SELECT id INTO m_shovey_id FROM goiardi.shoveys WHERE run_id = m_run_id;
-	    INSERT INTO goiardi.shovey_runs (shovey_run_id, shovey_id, node_name, status, ack_time, end_time, output, error, stderr, exit_status) VALUES (m_shovey_run_id, m_shovey_id, m_node_name, m_status, m_ack_time, m_end_time, m_output, m_stderr, m_exit_status);
+	    SELECT id INTO m_shovey_id FROM goiardi.shoveys WHERE run_id = m_shovey_run_id;
+	    INSERT INTO goiardi.shovey_runs (shovey_run_id, shovey_id, node_name, status, ack_time, end_time, output, error, stderr, exit_status) VALUES (m_shovey_run_id, m_shovey_id, m_node_name, m_status, m_ack_time, m_end_time, m_output, m_error, m_stderr, cast(m_exit_status as smallint));
 	EXCEPTION WHEN unique_violation THEN
 	    -- meh.
 	END;
