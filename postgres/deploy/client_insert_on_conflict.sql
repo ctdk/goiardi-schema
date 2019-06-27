ec2cae82faf6bc0dd63d48a343afbc52c2b3ddc0
ctdk <jbingham@gmail.com>
2019-06-27 09:18:44 -0700

    merge_clients using INSERT ... ON CONFLICT

---
+++ postgres/deploy/client_insert_on_conflict.sql
@@ -0,0 +1,24 @@
+-- Deploy goiardi_postgres:client_insert_on_conflict to pg
+
+BEGIN;
+
+CREATE OR REPLACE FUNCTION goiardi.merge_clients(m_name text, m_nodename text, m_validator boolean, m_admin boolean, m_public_key text, m_certificate text, m_organization_id bigint) RETURNS VOID AS
+$$
+DECLARE
+    u_id bigint;
+    u_name text;
+BEGIN
+    SELECT id, name INTO u_id, u_name FROM goiardi.users WHERE name = m_name;
+    IF FOUND THEN
+        RAISE EXCEPTION 'a user with id % named % was found that would conflict with this client', u_id, u_name;
+    END IF;
+
+    INSERT INTO goiardi.clients (name, nodename, validator, admin, public_key, certificate, created_at, updated_at, organization_id)
+        VALUES (m_name, m_nodename, m_validator, m_admin, m_public_key, m_certificate, NOW(), NOW(), m_organization_id)
+        ON CONFLICT(organization_id, name)
+            DO UPDATE SET nodename = m_nodename, validator = m_validator, admin = m_admin, public_key = m_public_key, certificate = m_certificate, updated_at = NOW();
+END;
+$$
+LANGUAGE plpgsql;
+
+COMMIT;
---
+++ postgres/verify/client_insert_on_conflict.sql
@@ -0,0 +1,7 @@
+-- Verify goiardi_postgres:client_insert_on_conflict on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
