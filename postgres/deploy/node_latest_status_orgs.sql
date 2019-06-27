1d96ee8140d40233b5cdcffdcf4fd2b31aac4c91
ctdk <jbingham@gmail.com>
2019-06-26 18:25:17 -0700

    Add organization_id to the node_latest_statuses view

---
+++ postgres/deploy/node_latest_status_orgs.sql
@@ -0,0 +1,37 @@
+-- Deploy goiardi_postgres:node_latest_status_orgs to pg
+
+BEGIN;
+
+DROP VIEW goiardi.node_latest_statuses;
+
+CREATE OR REPLACE VIEW goiardi.node_latest_statuses(
+	id,
+	name,
+	chef_environment,
+	organization_id,
+	run_list,
+	automatic_attr,
+	normal_attr,
+	default_attr,
+	override_attr,
+	is_down,
+	status,
+	updated_at)
+AS
+SELECT DISTINCT ON (n.id)
+	n.id,
+	n.name,
+	n.chef_environment,
+	n.organization_id,
+	n.run_list,
+	n.automatic_attr,
+	n.normal_attr,
+	n.default_attr,
+	n.override_attr,
+	n.is_down,
+	ns.status,
+	ns.updated_at
+	FROM goiardi.nodes n INNER JOIN goiardi.node_statuses ns ON n.id = ns.node_id
+	ORDER BY n.id, ns.updated_at DESC;
+
+COMMIT;
---
+++ postgres/verify/node_latest_status_orgs.sql
@@ -0,0 +1,7 @@
+-- Verify goiardi_postgres:node_latest_status_orgs on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
