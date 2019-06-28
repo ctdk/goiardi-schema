e19eaa313027ab38174404152a6c6d7f984d7b1c
ctdk <jbingham@gmail.com>
2019-06-28 08:53:15 -0700

    the updated merge_orgs function still had the full_name column & m_full_name variable referenced a couple of places. Also it ended up not having been updated with the insert ... on conflict stuff anyway.

--- postgres/deploy/org_new_columns.sql
+++ postgres/deploy/org_new_columns.sql
@@ -10,22 +10,28 @@ DROP FUNCTION goiardi.merge_orgs(m_name text, m_description text);
 CREATE OR REPLACE FUNCTION goiardi.merge_orgs(m_name text, m_description text, m_guid UUID, m_uuid bytea) RETURNS VOID AS
 $$
 BEGIN
-    LOOP
-        -- first try to update the key
-	UPDATE goiardi.organizations SET full_name = m_full_name, guid = m_guid, uuid = m_uuid, updated_at = NOW() WHERE name = m_name;
-	IF found THEN
-	    RETURN;
-	END IF;
-        -- not there, so try to insert the key
-        -- if someone else inserts the same key concurrently,
-        -- we could get a unique-key failure
-        BEGIN
-	    INSERT INTO goiardi.organizations (name, description, guid, uuid, created_at, updated_at) VALUES (m_name, m_description, m_guid, m_uuid, NOW(), NOW());
-            RETURN;
-        EXCEPTION WHEN unique_violation THEN
-            -- Do nothing, and loop to try the UPDATE again.
-        END;
-    END LOOP;
+    INSERT INTO goiardi.organizations (
+        name,
+        description,
+        guid,
+        uuid,
+        created_at,
+        updated_at
+    ) 
+        VALUES (
+            m_name,
+            m_description,
+            m_guid,
+            m_uuid,
+            NOW(),
+            NOW()
+        )
+        ON CONFLICT(name)
+            DO UPDATE SET
+                description = m_description,
+                guid = m_guid,
+                uuid = m_uuid,
+                updated_at = NOW();
 END;
 $$
 LANGUAGE plpgsql;
