163d190d768bd2622fc42bf06021a20f7e45e87b
ctdk <jbingham@gmail.com>
2020-04-22 18:22:03 -0700

    tweaking the policyfile related tables a little bit more

--- postgres/deploy/policy_groups.sql
+++ postgres/deploy/policy_groups.sql
@@ -17,12 +17,16 @@ CREATE TABLE goiardi.policy_groups (
 
 CREATE TABLE goiardi.policy_groups_to_policies (
 	id BIGSERIAL,
-	policy_id BIGINT NOT NULL,
 	pg_id BIGINT NOT NULL,
+	policy_id BIGINT NOT NULL,
+	policy_rev_id BIGINT NOT NULL,
 	created_at TIMESTAMP WITH TIME ZONE NOT NULL,
 	updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
 	PRIMARY KEY(id),
 	UNIQUE KEY(policy_id, pg_id, id),
+	FOREIGN KEY(policy_rev_id)
+		REFERENCES goiardi.policy_revisions(id)
+		ON DELETE CASCADE,
 	FOREIGN KEY(policy_id)
 		REFERENCES goiardi.policies(id)
 		ON DELETE CASCADE,
--- postgres/verify/policy_groups.sql
+++ postgres/verify/policy_groups.sql
@@ -4,7 +4,7 @@ BEGIN;
 
 SELECT id, name, organization_id, created_at, updated_at FROM goiardi.policy_groups WHERE FALSE;
 
-SELECT id, policy_id, pg_id, created_at, updated_at FROM goiardi.policy_groups_to_policies WHERE FALSE;
+SELECT id, pg_id, policy_id, policy_rev_id, created_at, updated_at FROM goiardi.policy_groups_to_policies WHERE FALSE;
 
 -- XXX Add verifications here.
 
